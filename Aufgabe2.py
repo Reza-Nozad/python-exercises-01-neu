32367a915c9be7bfdc01b99983668acf1e9ebeb1
reza <reza.nozad@gmail.com>
2024-10-01 15:20:41 +0200

    Aufgabe2 ist erledigt

---
+++ Aufgabe2.py
@@ -0,0 +1,8 @@
+a=10
+b=3
+print("Die Summe von a und b =", a+b)
+print("Die Differenz von a und b", a-b)
+print("Das Produkt von a und b", a*b)
+print("Den ganzzahligen Quotienten von a und b =", a//b)
+print("Den Rest der Division von a und b", a%b)
+print("Den Wert von a hoch b =", a**b)
