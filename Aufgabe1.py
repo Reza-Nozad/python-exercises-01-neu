6277141bc141c6b62f744d1a13358c3568c46429
reza <reza.nozad@gmail.com>
2024-10-01 15:10:18 +0200

    Aufgabe 1 erledigt

---
+++ Aufgabe1.py
@@ -0,0 +1,9 @@
+first_name="Reza"
+last_name="Nozad"
+birth_year="1988"
+has_license=True
+
+print("mein Vorname ist :", first_name)
+print("mein Nachname ist :", last_name)
+print("main Geburtsjahr ist :", birth_year)
+print("mein Führaerscheinstatus ist:", has_license)
